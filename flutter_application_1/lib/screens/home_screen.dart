9c9dcec2eaac83133f3a0a57a03ccf0d8229ff7e
danielle225 <danielle.domoraud@epitech.eu>
2024-09-17 12:54:13 +0000

    screen home and login

--- flutter_application_1/lib/screens/home_screen.dart
+++ flutter_application_1/lib/screens/home_screen.dart
@@ -2,30 +2,32 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';  // Import de l'écran de connexion
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Accueil'),
+        title: const Text('Accueil'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Bienvenue sur l\'application',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 // Redirige vers l'écran de connexion
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
                 );
               },
-              child: Text('Se connecter'),
+              child: const Text('Se connecter'),
             ),
           ],
         ),
